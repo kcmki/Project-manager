aad5c61f8812a8a84c9c778f0650238ada7d65f8
kcmki <mekkimohe@gmail.com>
2024-01-25 20:00:47 +0100

    tt

--- src/items/Prédiction.jsx
+++ src/items/Prédiction.jsx
@@ -6,7 +6,7 @@ import {Oval} from 'react-loader-spinner'
 import { useState , useEffect , useRef } from "react";
 import PrjctTasks from "./PrjctTasks";
 import MyPrjct from "./MyPrjct";
-import "./css/Content/Prédiction.css"
+import "./css/Content/prédiction.css"
 import arrow from './assets/4705847.png'
 function Prédiction({Terrain}){
     let ListeFruit = ["Pomme","Banane","Orange","Mandarine","Pamplemousse","Kiwi","Mangue","Ananas","Papaye","Raisin","Fraise","Framboise","Myrtille","Cerise","Abricot","Pêche","Nectarine","Poire","Prune","Raisin sec","Dattes","Figues","Grenade","Melon","Pastèque","Ramboutan","Litchi","Carambole","Kumquat","Figue de barbarie","Durian","Maracuja","Physalis","Noix de coco","Noix de cajou","Amandes","Pistaches","Noisettes","Châtaignes","Baies de Goji","Açaï","Mangoustan","Jacquier","Pomelo","Tamarin","Anone","Cerise de terre","Feijoa","Pitahaya","Nectar de coco","Tomate","Carotte","Pomme de terre","Concombre","Courgette","Poivron","Aubergine","Oignon","Ail","Brocoli","Chou-fleur","Chou","Laitue","Épinard","Bette à carde","Artichaut","Asperge","Haricot vert","Pois","Fève","Courge","Potiron","Patate douce","Navet","Radis","Betterave","Panais","Topinambour","Fenouil","Endive","Champignons","Céleri","Gingembre","Citrouille","Scorsonère","Salsifis","Cresson","Roquette","Bettes à cardes","Rutabaga","Chayote","Feuille de moutarde","Feuille de chou vert","Courge spaghetti","Chou-rave","Pissenlit","Raifort","Arroche","Cynorhodon","Sauge.",]
